3126d8727c5f2ffb7d0eead5a047f054b742fdb7
Manuj Sharma <manujsharmano1@gmail.com>
2020-03-02 09:37:04 +0530

    Update MainActivity.java
    changed dbHelper to dbHelpe (test)

--- TodoApp/app/src/main/java/com/example/manuj/todoapp/MainActivity.java
+++ TodoApp/app/src/main/java/com/example/manuj/todoapp/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -15,7 +14,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    DbHelpler dbHelpler;
+    DbHelpler dbHelple;
     ArrayAdapter mAdapter;
     ListView listView;
 
